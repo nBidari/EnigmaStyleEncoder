2a9cb2f5ca4b41d189cd0e265493255f78811077
Nima Bidari <nima.bidari@Y22-Design-Nima-Bidari.local>
2019-01-14 11:49:46 -0500

    justin told me to do this nothing actually happened

--- engimaEncoder.py
+++ engimaEncoder.py
@@ -31,44 +31,45 @@ class UserInterface:
 		self.rotorFrame = tk.LabelFrame(self.root, text = "Rotor #")
 		self.rotorFrame.config(background = "#374140", relief = "solid")
 		self.rotorFrame.grid(row = 0, column = 0, padx = 10, pady = 5)
-'''
-		self.warningLabel = tk.Label(self.rotorFrame, text = "Warning: You Can't Have Two or More of the Same Rotor's!")
-		self.warningLabel.config(background = "#374140", fg = "#374140")
-'''
-		self.rotorOneSpin = tk.Spinbox(self.rotorFrame, from_ = 0, to = 26, width = 3)
+
+		self.rotorOneSpin = tk.Spinbox(self.rotorFrame, from_ = 0, to = 5, width = 3)
 		self.rotorOneSpin.config(background = "#374140", fg = "#DC3522")
 		self.rotorOneSpin.grid(row = 1, column = 0, padx = 10, pady = 5)
 
-		self.rotorTwoSpin = tk.Spinbox(self.rotorFrame, from_ = 0, to = 26, width = 3)
+		self.rotorTwoSpin = tk.Spinbox(self.rotorFrame, from_ = 0, to = 5, width = 3)
 		self.rotorTwoSpin.config(background = "#374140", fg = "#DC3522")
 		self.rotorTwoSpin.grid(row = 1, column = 1, padx = 10, pady = 5)
 
-		self.rotorThreeSpin = tk.Spinbox(self.rotorFrame, from_ = 0, to = 26, width = 3)
+		self.rotorThreeSpin = tk.Spinbox(self.rotorFrame, from_ = 0, to = 5, width = 3)
 		self.rotorThreeSpin.config(background = "#374140", fg = "#DC3522")
 		self.rotorThreeSpin.grid(row = 1, column = 2, padx = 10, pady = 5)
+
+
+		#-------------------------------------------------------------------------------
+
+		self.posFrame = tk.LabelFrame(self.root, text = "Rotor Pos")
+		self.posFrame.config(background = "#374140", relief = "solid")
+		self.posFrame.grid(row = 1, column = 0, padx = 10, pady = 5)
+
+		self.posOneSpin = tk.Spinbox(self.posFrame, from_=0, to = 26, width = 3)
+		self.posOneSpin.config(background = "#374140", fg = "#DC3522")
+		self.posOneSpin.grid(row = 0, column = 0, padx = 10, pady = 5)
+
+		self.posTwoSpin = tk.Spinbox(self.posFrame, from_=0, to = 26, width = 3)
+		self.posTwoSpin.config(background = "#374140", fg = "#DC3522")
+		self.posTwoSpin.grid(row = 0, column = 1, padx = 10, pady = 5)
+
+		self.posThreeSpin = tk.Spinbox(self.posFrame, from_=0, to = 26, width = 3)
+		self.posThreeSpin.config(background = "#374140", fg = "#DC3522")
+		self.posThreeSpin.grid(row = 0, column = 2, padx = 10, pady = 5)
 
 		#Plugboard------------------------------------------------------------------------------------------------------
 
 		#Input/Output---------------------------------------------------------------------------------------------------
 
+		self.
 
 		self.root.mainloop()
-'''
-	def sameRotorCheck(self):
-		rotorOne = int(self.rotorOneSpin.get())
-		rotorTwo = int(self.rotorTwoSpin.get())
-		rotorThree = int(self.rotorThreeSpin.get())
-
-		while True:
-			if rotorOne == rotorTwo or rotorTwo == rotorThree or rotorOne == rotorThree:
-				self.warningLabel.config(fg = "#DC3522")
-			else:
-				self.warningLabel.config(fg = "#374140")
-'''
-'''
-	rotorCheck = sameRotorCheck()
-	rotorCheck = threading.Thread(target = sameRotorCheck())
-'''
 
 mainPage = UserInterface()
 
