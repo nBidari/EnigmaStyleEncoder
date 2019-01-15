9bedde8d53ebd7747d01f0fc2251c4047cead4af
Nima Bidari <nima.bidari@Y22-Design-Nima-Bidari.local>
2019-01-14 20:10:09 -0500

    GUI complete

--- engimaEncoder.py
+++ engimaEncoder.py
@@ -4,18 +4,35 @@
 import tkinter as tk
 import threading
 
+alphabet = "ABCDEFGHIJKMNOPQRSTUVWXYZ"
+
 class Rotor:
 	
-	def __init__(self,rotorOne, rotorTwo, rotorThree, posOne, posTwo, posThree):
-		self.rotorOne = rotorOne
-		self.rotorTwo = rotorTwo
-		self.rotorThree = rotorThree
+	def __init__(self,numRotorOne, numRotorTwo, numRotorThree,rotorOne, rotorTwo,rotorThree, rotorFour, posOne, posTwo, posThree):
+		self.numRotorOne = numRotorOne
+		self.numRotorTwo = numRotorTwo
+		self.numRotorThree = numRotorThree
+
+		self.numrotorOneRotate = 0
+		self.numrotorTwoRotate = 0
+		self.numrotorThreeRotate = 0
+
+		self.rotorOne = "JGDQOXUSCAMIFRVTPNEWKBLZYH"
+		self.rotorTwo = "NTZPSFBOKMWRCJDIVLAEYUXHGQ"
+		self.rotorThree = "JVIUBHTCDYAKEQZPOSGXNRMWFL"
+		self.rotorFour = "QYHOGNECVPUZTFDJAXWMKISRBL"
+		self.rotorFive = "QWERTZUIOASDFGHJKPYXCVBNML"
 
 		self.posOne = posOne
 		self.posTwo = posTwo
 		self.posThree = posThree
 
-class Plugs:
+	def rotorEncode:
+		print("Encode")
+	def rotorSpin:
+		print("Spin")
+
+class Plugboard:
 	pass
 
 class UserInterface:
@@ -65,11 +82,78 @@ class UserInterface:
 
 		#Plugboard------------------------------------------------------------------------------------------------------
 
+		self.plugWidth = 3
+
+		self.plugFrame = tk.LabelFrame(self.root, text = "Plugboard")
+		self.plugFrame.config(background = "#374140", fg = "#fffdd0", relief = "solid")
+		self.plugFrame.grid(row = 2, column = 0, rowspan = 6, padx = 10, pady = 5)
+
+		#-------------------------------------
+		self.entOne = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entOne.config(background = "#374140", fg = "#DC3522")
+		self.entOne.grid(row = 0, column = 0, padx = 10, pady = 5)
+
+		self.entTwo = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entTwo.config(background = "#374140", fg = "#DC3522")
+		self.entTwo.grid(row = 0, column = 1, padx = 10, pady = 5)
+
+		self.entThree = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entThree.config(background = "#374140", fg = "#DC3522")
+		self.entThree.grid(row = 0, column = 2, padx = 10, pady = 5)
+		
+		#-------------------------------------
+		self.entFour = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entFour.config(background = "#374140", fg = "#DC3522")
+		self.entFour.grid(row = 1, column = 0, padx = 10, pady = 5)
+
+		self.entFive = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entFive.config(background = "#374140", fg = "#DC3522")
+		self.entFive.grid(row = 1, column = 1, padx = 10, pady = 5)
+
+		self.entSix = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entSix.config(background = "#374140", fg = "#DC3522")
+		self.entSix.grid(row = 1, column = 2, padx = 10, pady = 5)
+
+		#-------------------------------------
+		self.entSeven = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entSeven.config(background = "#374140", fg = "#DC3522")
+		self.entSeven.grid(row = 2, column = 0, padx = 10, pady = 5)
+
+		self.entEight = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entEight.config(background = "#374140", fg = "#DC3522")
+		self.entEight.grid(row = 2, column = 1, padx = 10, pady = 5)
+
+		self.entNine = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entNine.config(background = "#374140", fg = "#DC3522")
+		self.entNine.grid(row = 2, column = 2, padx = 10, pady = 5)
+
+		#-------------------------------------
+		self.entTen = tk.Entry(self.plugFrame, width = self.plugWidth)
+		self.entTen.config(background = "#374140", fg = "#DC3522")
+		self.entTen.grid(row = 3, column = 1, padx = 10, pady = 5)
+
+
 		#Input/Output---------------------------------------------------------------------------------------------------
 
-		self.
+		self.inputEnt = tk.Text(self.root, background = "black", foreground = "lime",
+									height = 10, width = 65)
+		self.inputEnt.config(highlightbackground = "#074C0A")
+		self.inputEnt.grid(row = 0, column = 1, rowspan = 3, sticky = 'NESW', padx = 10, pady = 5)
+
+		self.submitbtn = tk.Button(self.root, text = "Encode", width = 25, height = 2)
+		self.submitbtn.config(background = "#DADFDF", relief = "raised", highlightbackground = "#374140")
+		self.submitbtn.grid(row = 4, column = 1, sticky = 'NESW',padx = 10, pady = 5)
+
+		self.outputEnt = tk.Text(self.root, background = "black", foreground = "lime",
+									height = 10, width = 65)
+		self.outputEnt.config(highlightbackground = "#074C0A")
+		self.outputEnt.grid(row = 5, column = 1, rowspan = 3, sticky = 'NESW', padx = 10, pady = 5)
+
+		#---------------------------------------------------------------------------------------------------------------
 
 		self.root.mainloop()
 
 mainPage = UserInterface()
+
+print(Rotor.rotorOne)
 
